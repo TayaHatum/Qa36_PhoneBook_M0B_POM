4da8564bd4c1be2b7bbd019b1653939c67f2ebd3
TayaHatum <21taya21@gmail.com>
2023-02-21 21:02:17 +0200

    added empty method for delete all

--- src/main/java/screens/ContactListScreen.java
+++ src/main/java/screens/ContactListScreen.java
@@ -2,7 +2,10 @@ package screens;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
@@ -32,14 +35,97 @@ public class ContactListScreen extends BaseScreen{
     List<MobileElement> contacts;
     @FindBy(id="android:id/button1")
     MobileElement yesButton;
+    int before;
+    int after;
 
-    public ContactListScreen removeOneContact(int count){
+    public int removeOneContact(int count){
         MobileElement contact = contacts.get(0);
        // MobileElement contact = contacts.get(count);
+        System.out.println("before -->"+contacts.size());
+        int before = contacts.size();
+        System.out.println("Screen Width:  " +driver.manage().window().getSize().getWidth());
+        System.out.println("Screen Height:  " +driver.manage().window().getSize().getHeight());
+        Rectangle rectangle = contact.getRect();
+
+        int x_a=rectangle.getX()+rectangle.getWidth()/8;
+        int y_a=rectangle.getY()+rectangle.getHeight()/2;
+
+        int x_b=rectangle.getX()+(rectangle.getWidth()/8)*7;
+        int y_b=y_a;
+
+
+        TouchAction<?> touchAction = new TouchAction<>(driver);
+        touchAction.longPress(PointOption.point(x_a,y_a))
+                .moveTo(PointOption.point(x_b,y_b)).release().perform();
+        should(yesButton,3);
+        yesButton.click();
+
+        pause(4000);
+        System.out.println("after -->"+contacts.size());
+        int after = contacts.size();
+        return before-after;
+    }
+    public ContactListScreen removeOneContactBase(int count){
+        //MobileElement contact = contacts.get(0);
+         MobileElement contact = contacts.get(count);
+         before=contacts.size();
+        System.out.println("before -->"+contacts.size());
+        System.out.println("Screen Width:  " +driver.manage().window().getSize().getWidth());
+        System.out.println("Screen Height:  " +driver.manage().window().getSize().getHeight());
+        Rectangle rectangle = contact.getRect();
+
+        int x_a=rectangle.getX()+rectangle.getWidth()/8;
+        int y_a=rectangle.getY()+rectangle.getHeight()/2;
+
+        int x_b=rectangle.getX()+(rectangle.getWidth()/8)*7;
+        int y_b=y_a;
+
+
+        TouchAction<?> touchAction = new TouchAction<>(driver);
+        touchAction.longPress(PointOption.point(x_a,y_a))
+                .moveTo(PointOption.point(x_b,y_b)).release().perform();
+        should(yesButton,3);
+        yesButton.click();
+
+        pause(4000);
+        System.out.println("after -->"+contacts.size());
+        after =contacts.size();
+        return this;
+    }
+    public ContactListScreen isListSizeLessOne(){
+
+        Assert.assertEquals(before-after,1);
+        return this;
+    }
+    public ContactListScreen removeOneContactAssert(int count){
+        MobileElement contact = contacts.get(0);
+        // MobileElement contact = contacts.get(count);
+        System.out.println("before -->"+contacts.size());
+        int before = contacts.size();
+        System.out.println("Screen Width:  " +driver.manage().window().getSize().getWidth());
+        System.out.println("Screen Height:  " +driver.manage().window().getSize().getHeight());
+        Rectangle rectangle = contact.getRect();
+
+        int x_a=rectangle.getX()+rectangle.getWidth()/8;
+        int y_a=rectangle.getY()+rectangle.getHeight()/2;
+
+        int x_b=rectangle.getX()+(rectangle.getWidth()/8)*7;
+        int y_b=y_a;
+
+
+        TouchAction<?> touchAction = new TouchAction<>(driver);
+        touchAction.longPress(PointOption.point(x_a,y_a))
+                .moveTo(PointOption.point(x_b,y_b)).release().perform();
+        should(yesButton,3);
+        yesButton.click();
+
+        pause(4000);
+        System.out.println("after -->"+contacts.size());
+        int after = contacts.size();
+        Assert.assertEquals(before-after,1);
 
         return this;
     }
-
     private void checkContacts(List<MobileElement> list,String text){
 
         boolean isPresent = false;
--- src/test/java/tests/RemoveContactTests.java
+++ src/test/java/tests/RemoveContactTests.java
@@ -2,7 +2,9 @@ package tests;
 
 import config.AppiumConfig;
 import models.Auth;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import screens.AuthenticationScreen;
 import screens.ContactListScreen;
@@ -17,11 +19,37 @@ public class RemoveContactTests extends AppiumConfig {
                 .isContactListActivityDisplayed();
     }
 
-    //providerContactData
+    @BeforeMethod
+    public void providerContacts() {
+        //providerContactData: check count of contacts & if count<2 --->add 2 contacts
+    }
 
     @Test
     public void removeOneContactSuccess(){
-        new ContactListScreen(driver)
+        int result = new ContactListScreen(driver)
                 .removeOneContact(2);
+        Assert.assertEquals(result,1);
+    }
+
+
+    @Test
+    public void removeOneContactSuccessAssert(){
+        new ContactListScreen(driver)
+                .removeOneContactAssert(2);
+
+    }
+
+    @Test
+    public void removeOneContactSuccess1(){
+        new ContactListScreen(driver)
+                .removeOneContactBase(1)
+                .isListSizeLessOne();
+    }
+
+    @Test
+    public void removeAllContacts(){
+        new ContactListScreen(driver)
+                .removeAllContact()
+                .isNoContactHere();
     }
 }
